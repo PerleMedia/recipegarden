05078470a625bbd20317a5e9738146b7369bcdb7
Jinju Loeding <jloeding@originate.ie>
2023-06-06 16:41:10 +0100

    migration complete

--- footer.php
+++ footer.php
@@ -21,17 +21,7 @@
 		<section id="footer-socket">
 			<div class="site-container">
 				<div class="site-info">
-					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'perlemedia' ) ); ?>">
-						<?php
-						/* translators: %s: CMS name, i.e. WordPress. */
-						printf( esc_html__( 'Proudly powered by %s', 'perlemedia' ), 'WordPress' );
-						?>
-					</a>
-					<span class="sep"> | </span>
-						<?php
-						/* translators: 1: Theme name, 2: Theme author. */
-						printf( esc_html__( 'Theme: %1$s by %2$s.', 'perlemedia' ), 'perlemedia', '<a href="http://underscores.me/">Underscores.me</a>' );
-						?>
+					<!-- Made with love by PerleMedia -->
 				</div><!-- .site-info -->
 				
 			</div><!-- .site-container -->
